6c9df97c87f8617d4662d78918bdfe4ced3f3886
Sara Ginsburg <sara.ginsburg@flatironschool.com>
2022-05-11 12:01:26 -0400

    Fetch with Forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector ('form');
+  inputForm.addEventListener ('submit', e => {
+    e.preventDefault ();
 
-document.addEventListener('DOMContentLoaded', init);
+    const input = document.querySelector ('input#searchByID');
+    const title = document.querySelector ('section#movieDetails h4');
+    const summary = document.querySelector ('section#movieDetails p');
+
+    fetch (`http://localhost:3000/movies/${input.value}`)
+      .then (resp => resp.json ())
+      .then (data => {
+        title.innerHTML = data.title;
+        summary.innerHTML = data.summary;
+      });
+  });
+};
+
+document.addEventListener ('DOMContentLoaded', init);
